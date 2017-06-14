18c887c4d07e63a111acdc377d14299011c5bfa3
Graham Benton <github.gpbenton@xoxy.net>
2017-06-14 15:00:37 +0100

    initial micalagg commit

---
+++ micalagg/allCalendars.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "iCalcreator.class.php";
+$errorlog = "errors.log";
+$aggconfig    = array( "unique_id" => "-//hewnsw//micalagg-",
+                    "directory" =>".",
+                    "filename"=>"icalmerge.ics" );
+
+$aggcalendar = new vcalendar( $aggconfig);
+
+if ($aggcalendar->useCachedCalendar()) {
+    exit();
+}
+
+$calendars = array("http://applications.huntingdonshire.gov.uk/applications/RefuseCalendarMVC/Home/Download/cal100090110283.ics",
+             "http://www.gov.uk/bank-holidays/england-and-wales.ics"
+             );
+
+
+
+foreach ($calendars as &$inputcalendar) {
+
+   $config = array ("unique_id" => "-//hewnsw//micalagg-",
+                             "url" => $inputcalendar);
+
+   $vcalendar = new vcalendar($config);
+   $vcalendar->parse();
+   while ( $event = $vcalendar->getComponent("vevent")) {
+      $aggcalendar->setComponent($event);
+   }
+
+}
+
+$aggcalendar->sort();
+
+$aggcalendar->saveCalendar();
+
+$aggcalendar->returnCalendar();
+
+?>
